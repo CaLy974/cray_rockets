8c3bd33330b401480f49b915edb39a404173c843
Gohar Vardanyan <gohsamvelyan@gmail.com>
2022-11-22 12:22:00 +0100

    Resolved problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root to: "rockets#home"
-  sources :rockets, only: [:show]
   # get "rockets/:id", to "rockets#show"
-
-=======
   devise_for :users
-  root to: "pages#home"
-  resources :rockets, only: [:index]
->>>>>>> origin
+
+  resources :rockets, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
