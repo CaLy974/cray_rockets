d62dcb98750a5b004c03789e51724dbb8f8aa977
Lisa Marchetti <lisa3marchetti@gmail.com>
2022-11-23 14:55:32 +0100

    new create et commencement edit et delete

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -1,4 +1,5 @@
 class Rocket < ApplicationRecord
   belongs_to :user
   validates :name, :town, :capacity, :price, presence: true # :country à ajouter
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.destroy_all
+require "open-uri"
+
 Rocket.destroy_all
+User.destroy_all
 
 user1 = User.create!(email: "toto@gmail.com", password: "tototo")
 
-rocket1 = Rocket.create!(name: "big mama", user: user1, capacity: "6", country: "France", town: "Toulon", price: 12_000_00)
+file = URI.open("https://res.cloudinary.com/debuijcke/image/upload/v1669200861/Rockets/Fus%C3%A9e8_whpgb1.png")
+rocket1 = Rocket.create!(name: "Big mama", user: user1, capacity: "6", country: "France", town: "Toulon", price: 12_000_00)
+
+rocket1.photo.attach(io: file, filename: "rocket1.png", content_type: "image/png")
+rocket1.save
